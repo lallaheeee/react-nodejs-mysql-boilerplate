dbc48d857978b64f477acb6e4bf06309dca8f811
lallaheeee <ooop0422@gmail.com>
2020-01-12 16:34:12 +0900

    [feat] eslint no-console 룰로 인한 커스텀 console 작성
    실수로 콘솔을 찍는 걸 방지해주는 좋은 규칙이라 생각한다
    꼭 출력해야할 건 커스텀 콘솔을 이용해보자

---
+++ server/utils/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+const Console = {
+  log: (...args) => {
+    console.log(...args);
+  },
+  error: (...args) => {
+    console.error(...args);
+  },
+};
+
+export { Console };
