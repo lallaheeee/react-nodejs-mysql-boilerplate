5216dffe481e0b8b18d6141d16bc4d13bf453a0c
lallaheeee <ooop0422@gmail.com>
2020-01-14 17:30:04 +0900

    [feat] 컨트롤러 예시 작성
    이제 도메인 예시로 유저를 바꾼 후
    인증을 추가해보자 ㅎㅎ

--- server/domain/controller.js
+++ server/domain/controller.js
@@ -1,21 +1,51 @@
-const preloadHandler = (req, res, next) => {
-  next();
+import db from '../config/database';
+
+const preloadHandler = (req, res, next, domainId) => {
+  const { Domain } = db;
+
+  Domain.findByPk(domainId)
+    .then(domain => {
+      if (!domain) return res.sendStatus(404);
+
+      req.domain = domain;
+      return next();
+    })
+    .catch(() => res.sendStatus(500));
 };
 
-const postHandler = (_, res) => {
-  res.send('this is postHandler!');
+const postHandler = (req, res) => {
+  const { Domain } = db;
+
+  const { title, active } = req.body;
+  Domain.create({ title, active })
+    .then(domain => res.send(domain))
+    .catch(() => res.sendStatus(500));
 };
 
-const getHandler = (_, res) => {
-  res.send('this is getHandler!');
+const getHandler = (req, res) => {
+  const { domain } = req;
+  res.send(domain);
 };
 
-const putHandler = (_, res) => {
-  res.send('this is putHandler!');
+const putHandler = (req, res) => {
+  const { domain } = req;
+  const { title, active } = req.body;
+
+  if (title) domain.title = title;
+  if (active) domain.active = active;
+
+  domain
+    .save()
+    .then(saveDomain => res.send(saveDomain))
+    .catch(err => res.send(err));
 };
 
-const deleteHandler = (_, res) => {
-  res.send('this is deleteHandler!');
+const deleteHandler = (req, res) => {
+  const { domain } = req;
+  domain
+    .destroy()
+    .then(res.sendStatus(204))
+    .catch(() => res.sendStatus(500));
 };
 
 export {
