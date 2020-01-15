842839825228466d411ff25370aff7d7f3af8701
lallaheeee <ooop0422@gmail.com>
2020-01-15 14:10:14 +0900

    [feat] 글로벌 스타일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Home } from './page';
+import GlobalStyle from './components/GlobalStyles';
+import { Home } from './pages';
 
 const App = () => (
   <Router>
+    <GlobalStyle />
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
